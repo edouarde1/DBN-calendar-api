273f00f3ca505ecee8112ee5d6e7e59092df9233
VNAJack <veronica.jack73@gmail.com>
2021-12-08 16:30:33 -0800

    still a work in progress. Forgetful is now a hidden node

--- mk_needPrepTime.m
+++ mk_needPrepTime.m
@@ -28,13 +28,13 @@ inter = mk_adj_mat( intercons, names, 0);
 
 % observation nodes for 1 time step (Priority, Travel, StartTime)
 % technically also (Forgetful, NightOwl)
-onodes = [ 1 2 4 5 6 ];
+onodes = [ 1 2 5 6 ];
 
 % discretize nodes
 NPT     = 2;    % three states NPT = {low, med, high}
 P       = 2;    % two states P = {false, true}
 T       = 2;    % two states T = {false, true}
-F       = 3;    % three states F = {low, med, high}
+F       = 2;    % three states F = {false, true}
 A       = 3;    % three states A = {low, med, high}
 ST      = 3;    % three states ST = {morning, day, night}
 NO      = 2;    % two states NO = {false, true}
@@ -71,7 +71,8 @@ Forgetfulness1 = 11;
 % Prior Distribution: Pr( Forgetfulness0 )
 % Forgetfulness  F=low  F=med  F=high
 %                0.33     0.33     0.33
-cpt = [1/3 1/3 1/3];
+cpt = [0.5 0.5];
+%cpt = [1/3 1/3 1/3]; % if it had three values
 bnet.CPD{Forgetfulness0} = tabular_CPD( bnet, Forgetfulness0, 'CPT', cpt );
 
 % Transition Function: Pr( Forgetfulness_t | Forgetfulness_t-1 )
@@ -79,9 +80,15 @@ bnet.CPD{Forgetfulness0} = tabular_CPD( bnet, Forgetfulness0, 'CPT', cpt );
 % low   0.90    0.09    0.01        F = low, so you remember a lot
 % med   0.05    0.90    0.05        
 % high  0.01    0.09    0.90        F = high, so you forget everything
-cpt = [0.90 0.05 0.01 ...
-       0.09 0.90 0.09 ...
-       0.01 0.05 0.90];
+
+% F0    F1=false F1=true
+% false 0.90     0.10
+% true  0.02     0.98
+cpt = [.90 .02 .10 .98];
+% CPT if forgetfulness has three values
+%cpt = [0.90 0.05 0.01 ...
+%       0.09 0.90 0.09 ...
+%       0.01 0.05 0.90];
 bnet.CPD{Forgetfulness1} = tabular_CPD( bnet, Forgetfulness1, 'CPT', cpt );
 
 % Prior distribution: Pr(Priority)
@@ -125,14 +132,15 @@ bnet.CPD{Alertness} = tabular_CPD(bnet, Alertness, 'CPT', cpt);
 % Pr( NeedPrepTime | Travel, Priority, Forgetfullness, Alertness)
 % Table too large to include in comments
 % Please read intuition section in report
-cpt =[0.68 0.78 0.85 0.55 0.65 0.72 0.45 0.55 0.62 ...
-      0.53 0.63 0.70 0.40 0.50 0.57 0.30 0.40 0.47 ...
-      0.43 0.53 0.60 0.30 0.40 0.47 0.20 0.30 0.37 ...
-      0.28 0.38 0.45 0.15 0.25 0.32 0.05 0.15 0.22 ...
-      0.32 0.22 0.15 0.45 0.35 0.28 0.55 0.45 0.38 ...
-      0.47 0.37 0.30 0.60 0.50 0.43 0.70 0.60 0.53 ...
-      0.57 0.47 0.40 0.70 0.60 0.53 0.80 0.70 0.63 ...
-      0.72 0.62 0.55 0.85 0.75 0.68 0.95 0.85 0.78];
+cpt = [0.68 0.78 0.85 0.45 0.55 0.62 0.53 0.63 0.7 0.3 0.4 0.47 0.43 0.53 0.6 0.2 0.3 0.37 0.28 0.38 0.45 0.05 0.15 0.22 0.32 0.22 0.15 0.55 0.45 0.38 0.47 0.37 0.3 0.7 0.6 0.53 0.57 0.47 0.4 0.8 0.7 0.63 0.72 0.62 0.55 0.95 0.85 0.78];
+%cpt =[0.68 0.78 0.85 0.55 0.65 0.72 0.45 0.55 0.62 ...
+%      0.53 0.63 0.70 0.40 0.50 0.57 0.30 0.40 0.47 ...
+%      0.43 0.53 0.60 0.30 0.40 0.47 0.20 0.30 0.37 ...
+%      0.28 0.38 0.45 0.15 0.25 0.32 0.05 0.15 0.22 ...
+%      0.32 0.22 0.15 0.45 0.35 0.28 0.55 0.45 0.38 ...
+%      0.47 0.37 0.30 0.60 0.50 0.43 0.70 0.60 0.53 ...
+%      0.57 0.47 0.40 0.70 0.60 0.53 0.80 0.70 0.63 ...
+%      0.72 0.62 0.55 0.85 0.75 0.68 0.95 0.85 0.78];
 bnet.CPD{NeedPrepTime} = tabular_CPD(bnet, NeedPrepTime, 'CPT', cpt);
 DBN = bnet;
 
--- sim_decision.m
+++ sim_decision.m
@@ -1,4 +1,4 @@
-function prNeedPrepTime = sim_decision(dbn, ex, isNightOwl, startTime, forgetful, travel, priority)
+function prNeedPrepTime = sim_decision(dbn, ex, isNightOwl, startTime, travel, priority)
 % function prNeedPrepTime = sim_decision(dbn, forgetful, isNightOwl)
 % ARGS: dbn = dynamic bayes net model specified by BNT syntax
 %       ex = setting used to generate evidence
@@ -8,38 +8,38 @@ function prNeedPrepTime = sim_decision(dbn, ex, isNightOwl, startTime, forgetful
 % Note: The user values forgetful and isNightOwl fix the evidence
 
 engine = bk_inf_engine(dbn); % set up inference engine
-T = 30; % 30 time steps to view patterns over time
+T = 30;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % generate a series of evidence in advance
+% To change the fixed variables, look for !! in comments below !!
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if ex == 1
+if ex == 1 % For flag 1, create random evidence
     disp('Creating random evidence...')
-    ev = sample_dbn(dbn,T); % creates random evidence from dbn cpts
-    evidence = cell(7,T);   % 7 nodes per slice, time steps
+    ev = sample_dbn( dbn, T ); % similar to randi
+    evidence = cell(7,T); % 3 nodes per slice, 50 time steps
     onodes = dbn.observed;
-    evidence(onodes,:) = ev(onodes,:); % all cells besides onodes are empty
-
-elseif ex == 2
+    evidence(onodes,:) = ev( onodes, : ); % all cells besides onodes are empty
+elseif ex == 2 % For flag 2, create fixed observable evidence
     fprintf(['Creating evidence with isNightOwl=%d, StartTime=%d, ' ...
-        'Forgetfulness=%d, Travel=%d, Priority=%d\n'], ...
-        isNightOwl, startTime, forgetful, travel, priority)
-    evidence = cell(7,T);
-    for ii = 1:T % Iterate through evidence and fix values to event info
+        'Travel=%d, Priority=%d\n'], ...
+        isNightOwl, startTime, travel, priority)
+    evidence = cell(7,T); % 7 nodes, T time steps
+    for ii = 1:2 % Iterate through evidence and fix values to event info
         evidence{1,ii} = isNightOwl;
         evidence{2,ii} = startTime;
-        evidence{4,ii} = forgetful;
         evidence{5,ii} = travel;
         evidence{6,ii} = priority;
     end
-
-else
-    NPT_fixed = 1; % NeedPrepTime={1=false, 2=true}
-    fprintf('Creating evidence with NeedPrepTime=%d', NPT_fixed)
-    evidence = sample_seq(dbn, NPT_fixed, Time);
+else % For flag 3, randomness is restricted by fixing the hidden variable
+    NPT_fixed = 1;
+    F_fixed = 1;
+    disp('Creating restricted evidence where NeedPrepTime=%d and Forgetfulness=%d', ...
+        NPT_fixed, F_fixed)
+    evidence = sampleHelp_seq( dbn, NPT_fixed, F_fixed, T );
 end
 
-% disp(evidence) % Can print out evidence - not pretty
+%disp(evidence) % print out evidence that gets generated (not pretty)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % inference process: infer if user needs help over T time steps
@@ -52,61 +52,64 @@ subplot(1, 2, 1); % setup plot for graph
 
 % at t=0, no evidence has been entered, so the probability is same as the
 % prior encoded in the DBN itself
-prForgetful = get_field(dbn.CPD{dbn.names('Forgetfulness')},'cpt');
-prForgetful
-belief = [belief, prForgetful(3)];
-subplot(1,2,1);
+
+prForgetful = get_field( dbn.CPD{ dbn.names('Forgetfulness') }, 'cpt' )
+belief = [belief, prForgetful(2)];
+subplot(1, 2, 1);
 plot(belief, 'o-', LineWidth=1)
 
 % log best decision
-[bestA, eu] = get_meu(prForgetful(3));
+[bestA, eu] = get_meu( prForgetful(2) );
 exputil = [exputil, eu];
-fprintf('t=%d: best action = %s, EU = %f\n', 0, bestA, eu);
-subplot(1,2,2);
-plot(exputil, '*-', LineWidth=1);
+fprintf('t=%d: best action = %s, eu = %f\n', 0, bestA, eu);
+subplot( 1, 2, 2 );
+plot( exputil, '*-', LineWidth=1);
 
-% At t=1, initialize the belief state
-[engine, ll(1)] = dbn_update_bel1(engine, evidence(:,1))
+% at t=1: initialize the belief state 
+[engine, ll(1)] = dbn_update_bel1(engine, evidence(:,1));
 
-marg = dbn_marginal_from_bel(engine, 1)
+marg = dbn_marginal_from_bel(engine, 1);
 prForgetful = marg.T
-belief = [belief, prForgetful(3)];
-subplot(1,2,1);
-plot(belief, 'o-', LineWidth=1);
+belief = [belief, prForgetful(2)];
+subplot( 1, 2, 1 );
+plot( belief, 'o-', LineWidth=1);
 
 % log best decision
-[bestA, eu] = get_meu(prForgetful(3));
+[bestA, eu] = get_meu( prForgetful(2) );
 exputil = [exputil, eu];
-fprintf('t=%d: best action = %s, EU = %f\n', 0, bestA, eu);
-subplot(1,2,2);
-plot(exputil, '*-', LineWidth=1);
+fprintf('t=%d: best action = %s, eu = %f\n', 0, bestA, eu);
+subplot( 1, 2, 2 );
+plot( exputil, '*-' );
 
 % Repeat inference steps for each time step
 for t=2:T
-    % update belief with evidence at current time step
-    [engine, ll(1)] = dbn_update_bel(engine, evidence(:,t-1:t));
+  % update belief with evidence at current time step
+  [engine, ll(t)] = dbn_update_bel(engine, evidence(:,t-1:t));
+  
+  % extract marginals of the current belief state
+  i = 1;
+  marg = dbn_marginal_from_bel(engine, i);
+  prForgetful = marg.T;
 
-    % extract marginals of current belief state
-    i = 1;
-    marg = dbn_marginal_from_bel(engine, i);
-    prForgetful = marg.T;
+  % log best decision
+  [bestA, eu] = get_meu( prForgetful(2) );
+  exputil = [exputil, eu]; 
+  fprintf('t=%d: best action = %s, eu = %f\n', t, bestA, eu);
+  subplot( 1, 2, 2 );
+  plot( exputil, '*-', LineWidth=1);
+  xlabel( 'Time Steps' );
+  ylabel( 'EU(Help)' );
+  axis( [ 0 T -5 5] );
 
-    % log best decision
-    [bestA, eu] = get_meu(prForgetful(3));
-    exputil = [exputil, eu];
-    fprintf('t=%d: best action = %s, EU = %f\n', 0, bestA, eu);
-    subplot(1,2,2);
-    plot(exputil, '*-', LineWidth=1);
-    xlabel('Time Steps');
-    ylabel('EU')
-    axis([0 T -5 5]); % limits of EU
-
-    % keep track of results and plot it
-    belief = [belief, prForgetful(3)];
-    subplot(1,2,1);
-    plot(belief, 'o-', LineWidth=1)
-    xlabel('Time Steps');
-    ylabel('Pr(Forgetfulness) = high');
-    axis([0 T 0 1]);
-    pause(0.01);
+  % keep track of results and plot it
+  belief = [belief, prForgetful(2)];
+  subplot( 1, 2, 1 );
+  plot( belief, 'o-' );
+  xlabel( 'Time Steps' );
+  ylabel( 'Pr(NeedHelp)' );
+  axis( [ 0 T 0 1] );
+  pause(0.01);
 end
+
+
+
